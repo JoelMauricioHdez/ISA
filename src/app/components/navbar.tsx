2ab93d76631fd5427c86efcf69ab415da1747f7c
JoelMauricioHdez <joel.mauricio@hecmapp.com>
2023-07-25 19:34:56 -0400

    fixes and addons pre-test

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -9,7 +9,11 @@ import IconUser from "./icons/UserIcon";
 import { useRouter } from "next/navigation";
 import IconLogoutBoxLine from "./icons/logout";
 
-export default function Navbar(){
+type NavbarProps = {
+    isProfile?:boolean
+}
+
+export default function Navbar({isProfile=false}:NavbarProps){
     const router = useRouter()
     const supabase = createClientComponentClient()
     const [user, setUser] = useState<User | undefined>()
@@ -31,6 +35,10 @@ export default function Navbar(){
         setIsOpen(!isOpen)
     }
 
+    const handleBack = () => {
+        router.back()
+    }
+
     const handleSession = async () => {
         const {error} = await supabase.auth.signOut()
         if (error){
@@ -41,10 +49,21 @@ export default function Navbar(){
 
     return (
         <nav className="flex w-screen h-[65px] bg-customBlack justify-between py-[5px] px-[20px] items-center p-5">
-            <Link href={'https://intec.edu.do'} prefetch={false}>
-                <Image src={'/logo-intec.png'} alt={'intec logo'} width={75} height={25} priority className="w-auto h-auto"/>
-            </Link>
-            <button className="flex items-center justify-between text-white gap-2 focus:outline-none" onClick={handleClick}><IconUser/>{user?.email?.split("@")[0]}</button>
+            {
+                isProfile ? (
+                    <>                
+                    <button className="flex items-center gap-1 w-fit text-white" onClick={handleBack}>Volver</button>
+                    <button className="flex items-center gap-1 w-fit text-white" onClick={handleSession}><IconLogoutBoxLine/>Cerrar Sesión</button>
+                    </>
+                ):(
+                <>
+                    <Link href={'https://intec.edu.do'} prefetch={false}>
+                        <Image src={'/logo-intec.png'} alt={'intec logo'} width={75} height={25} priority className="w-auto h-auto"/>
+                    </Link>
+                    <button className="flex items-center justify-between text-white gap-2 focus:outline-none" onClick={handleClick}><IconUser/>{user?.email?.split("@")[0]}</button>
+                </>
+                ) 
+            }
             <div className={`absolute z-20 flex flex-col w-[140px] h-[100px] bg-[#282C31] text-white text-sm items-center right-0 top-[60px] p-[15px] gap-4 rounded-bl-md ${isOpen ? "" : "hidden"}`} onMouseLeave={handleClick}>
                 <Link href={"/perfil"} className="flex items-center gap-1 w-full"><IconUser/>Ver perfil</Link>
                 <button className="flex items-center gap-1 w-full" onClick={handleSession}><IconLogoutBoxLine/>Cerrar Sesión</button>
